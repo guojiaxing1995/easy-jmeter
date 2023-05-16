247eac81e18cd1aa0566d86ac2a06725ec9bdd96
guojiaxing1995 <35020645+guojiaxing1995@users.noreply.github.com>
2023-05-16 18:00:15 +0800

    swagger配置

--- api/src/main/java/io/github/guojiaxing1995/easyJmeter/controller/v1/ProjectController.java
+++ api/src/main/java/io/github/guojiaxing1995/easyJmeter/controller/v1/ProjectController.java
@@ -2,6 +2,9 @@ package io.github.guojiaxing1995.easyJmeter.controller.v1;
 
 import io.github.guojiaxing1995.easyJmeter.model.ProjectDO;
 import io.github.guojiaxing1995.easyJmeter.service.ProjectService;
+import io.github.talelin.core.annotation.LoginRequired;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/project")
+@Api(tags = "项目管理")
 @Validated
 public class ProjectController {
 
@@ -20,6 +24,8 @@ public class ProjectController {
     private ProjectService projectService;
 
     @GetMapping("")
+    @ApiOperation(value = "项目查询", notes = "根据项目名称查询项目")
+    @LoginRequired
     public List<ProjectDO> search(@RequestParam(value = "name", required = false, defaultValue = "") String name){
         List<ProjectDO> projects = projectService.getProjectByName(name);
         return projects;
