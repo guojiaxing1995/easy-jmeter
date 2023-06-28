80507785f5fc2700fb33d47fc50f5ab8da678d64
guojiaxing1995 <35020645+guojiaxing1995@users.noreply.github.com>
2023-06-28 19:14:51 +0800

    集成socketio服务端

---
+++ api/src/main/java/io/github/guojiaxing1995/easyJmeter/common/configuration/NettySocketConfiguration.java
@@ -0,0 +1,48 @@
+package io.github.guojiaxing1995.easyJmeter.common.configuration;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration(proxyBeanMethods = false)
+public class NettySocketConfiguration {
+
+    @Value("${socket.host}")
+    private String host;
+
+    @Value("${socket.port}")
+    private int port;
+
+    @Value("${socket.enable}")
+    private boolean enableSocket;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+        config.setOrigin("*");
+        config.setAllowCustomRequests(true);
+
+        SocketIOServer server = new SocketIOServer(config);
+
+        if (enableSocket) {
+            server.start();
+            log.info("SocketIO server started on {}:{}", host, port);
+        } else {
+            log.info("SocketIO server is disabled.");
+        }
+
+        return server;
+    }
+
+    @Bean
+    public SpringAnnotationScanner springAnnotationScanner(SocketIOServer socketServer) {
+        return new SpringAnnotationScanner(socketServer);
+    }
+
+}
---
+++ api/src/main/java/io/github/guojiaxing1995/easyJmeter/controller/v1/SocketIOHandler.java
@@ -0,0 +1,42 @@
+package io.github.guojiaxing1995.easyJmeter.controller.v1;
+
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.OnConnect;
+import com.corundumstudio.socketio.annotation.OnDisconnect;
+import com.corundumstudio.socketio.annotation.OnEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class SocketIOHandler {
+
+    @Autowired
+    private SocketIOServer socketServer;
+
+    @OnConnect
+    public void onConnect(SocketIOClient client) {
+        log.info("Client connected: " + client.getSessionId());
+        // 处理连接事件
+        // ...
+    }
+
+    @OnDisconnect
+    public void onDisconnect(SocketIOClient client) {
+        log.info("Client disconnected: " + client.getSessionId());
+        // 处理断开连接事件
+        // ...
+    }
+
+    @OnEvent("msg")
+    public void handleMsgEvent(SocketIOClient client, String message) {
+        // 处理 chat 事件的逻辑
+        // ...
+        log.info("msg: " + message);
+        client.sendEvent("msg", "已经收到" + message);
+
+    }
+
+}
