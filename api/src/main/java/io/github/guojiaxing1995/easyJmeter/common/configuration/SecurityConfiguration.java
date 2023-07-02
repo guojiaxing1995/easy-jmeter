e8ef4f1541094a4757e4cf711545d061d6b007cf
guojiaxing1995 <35020645+guojiaxing1995@users.noreply.github.com>
2023-07-02 13:02:09 +0800

    控制客户端时服务不可被访问

---
+++ api/src/main/java/io/github/guojiaxing1995/easyJmeter/common/configuration/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package io.github.guojiaxing1995.easyJmeter.common.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${socket.server.enable}")
+    private boolean enableServer;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (!enableServer) {
+            http
+                    .authorizeRequests()
+                    .anyRequest().denyAll()
+                    .and()
+                    .csrf().disable();
+        }
+    }
+}
